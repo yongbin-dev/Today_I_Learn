72be5c6095d0d5dd4bb1092cda8539689b7fcaf3
albin <ybkim@aipim.io>
2023-12-13 15:35:19 +0900

    refactor :: delete project

--- react/react-api/README.md
+++
@@ -1,9 +0,0 @@
-# fastcampus 모던 리액트 API 
-
-### 00. 소개
-### 01. API 연동의 기본
-### 02. useReducer로 요청 상태 관리하기
-### 03. useAsync 커스텀 Hook 만들어서 사용하기
-### 04. react-async로 요청 상태 관리하기
-### 05. Context 에서 비동기작업 상태 관리하기
-### 06. Context에서 비동기 작업 상태 관리하기 - 리팩토링
--- react/react-redux-middleware/src/middleware/myLogger.js
+++
@@ -1,9 +0,0 @@
-const myLogger = (store) => (next) => (action) => {
-  console.log(action);
-  console.log("prev : \t", store.getState());
-  const result = next(action);
-  console.log("Next : \t", store.getState());
-  return result;
-};
-
-export default myLogger;

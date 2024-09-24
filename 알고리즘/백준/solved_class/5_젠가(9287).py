53b5afe751473c6e6b55b6e8408351dc8989a545
albin <ybkim@aipim.io>
2024-09-24 17:25:57 +0900

    .

--- 알고리즘/백준/solved_class/5_젠가(9287).py
+++ 알고리즘/백준/solved_class/5_젠가(9287).py
@@ -1,23 +1,10 @@
-n = int(input())
-
-students = [[0] * n for _ in range(n)]
-datas = [[int(x) for x in input().split()] for _ in range(n)]
-
-for i in range(n):
-    for j in range(5):
-        for k in range(n):
-            if datas[i][j] == datas[k][j]:
-                students[i][k] = 1
-
-answers = [0] * n
-for idx, s in enumerate(students):
-    print(s)
-    answers[idx] = s.count(1)
-
-def printAnswer(answers):
-    for i in range(n):
-        if answers[i] == max(answers):
-            print(i+1)
-            return
-
-printAnswer(answers)
+s = ["Standing", "Fallen"]
+T = int(input())
+for t in range(1, T + 1):
+    n = int(input())
+    fallen = 0
+    for _ in range(n):
+        block = input()
+        if block[1] == '0' and (block[0] == '0' or block[2] == '0'):
+            fallen = 1
+    print("Case", str(t) + ':', s[fallen])
